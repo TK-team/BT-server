3973127ad5268d604abd325426de17fc7256066a
Tristan-FZ-Xiao <tristan_xiao@qq.com>
2015-09-05 22:36:18 +0800

    unfinish: let me achieve the communication with Tracker Server feature first.

--- include/bt_task.h
+++ include/bt_task.h
@@ -7,7 +7,22 @@
 
 struct bt_task {
 	struct torrent *torrent_info;
-	struct list_head peer_list;
+	struct list_head unuse_peer_list;
+	struct list_head inuse_peer_list;
+	unsigned short listen_port;
+	struct b_string *info_hash;
+	struct b_string *peer_id;
+	unsigned long long downloaded;
+	unsigned long long left;
+	unsigned int compact;
+	int event;
+	int max_peer_num;
+	/* optional begin */
+	int ip;
+	unsigned int numwant;
+	int key;
+	int trackerid;
+	/* optional end */
 };
 
 #endif /* _BT_TASK_H_ */
